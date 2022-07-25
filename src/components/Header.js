800eb343022b66cbb67ef5231e38784545b399d5
Gustavo <guvono4@gmail.com>
2022-07-25 20:04:10 -0300

    correção requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,17 +28,19 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <h2>TrybeTunes</h2>
-        <p data-testid="header-user-name">
-          Bem vindo,
-          {' '}
-          {loading ? <p>Carregando...</p> : <p>{user.name}</p>}
-        </p>
         {loading ? <p>Carregando...</p> : (
-          <nav>
-            <Link to="/search" data-testid="link-to-search">Search </Link>
-            <Link to="/favorites" data-testid="link-to-favorites">Favorites </Link>
-            <Link to="/profile" data-testid="link-to-profile">Profile </Link>
-          </nav>
+          <p data-testid="header-user-name">
+            Bem vindo,
+            {' '}
+            <div>
+              <p>{user.name}</p>
+              <nav>
+                <Link to="/search" data-testid="link-to-search">Search </Link>
+                <Link to="/favorites" data-testid="link-to-favorites">Favorites </Link>
+                <Link to="/profile" data-testid="link-to-profile">Profile </Link>
+              </nav>
+            </div>
+          </p>
         )}
       </header>
     );
